b72c911bb894501de9ccc06f3820bdf3b925dd77
Clodoaldo Brasilino <886522+clodbrasilino@users.noreply.github.com>
2020-11-30 14:23:45 -0300

    WIP: footer

--- footer.php
+++ footer.php
@@ -2,21 +2,23 @@
     <footer>
       <div class="footer">
         <div class="social-bar">
-          <a href="#" class="nav-link">
-              <img class="social-icon" src="/wp-content/themes/ascension/images/youtube.png">
-          </a>
-          <a href="#" class="nav-link">
-              <img class="social-icon" src="/wp-content/themes/ascension/images/facebook.png">
-          </a>
-          <a href="#" class="nav-link">
-              <img class="social-icon" src="/wp-content/themes/ascension/images/instagram.png">
-          </a>
-          <a href="#" class="nav-link">
-              <img class="social-icon" src="/wp-content/themes/ascension/images/discord.png">
-          </a>
-          <a href="#" class="nav-link">
-              <img class="social-icon" src="/wp-content/themes/ascension/images/twitch.png">
-          </a>
+          <div class="social-links">
+            <a href="#" class="nav-link">
+                <img class="social-icon" src="/wp-content/themes/ascension/images/youtube.png">
+            </a>
+            <a href="#" class="nav-link">
+                <img class="social-icon" src="/wp-content/themes/ascension/images/facebook.png">
+            </a>
+            <a href="#" class="nav-link">
+                <img class="social-icon" src="/wp-content/themes/ascension/images/instagram.png">
+            </a>
+            <a href="#" class="nav-link">
+                <img class="social-icon" src="/wp-content/themes/ascension/images/discord.png">
+            </a>
+            <a href="#" class="nav-link">
+                <img class="social-icon" src="/wp-content/themes/ascension/images/twitch.png">
+            </a>
+          </div>
         </div>
         <div class="footer-shield"></div>
         <div class="built-by">Ascension Guild © 2020</a></div>
