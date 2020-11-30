8d72fdf2700462bb9877c1dfbe6b24327be522e1
Clodoaldo Brasilino <886522+clodbrasilino@users.noreply.github.com>
2020-11-30 12:25:39 -0300

    WIP: footer

--- footer.php
+++ footer.php
@@ -1,7 +1,9 @@
 
     <footer>
       <div class="footer">
+        <div class="social-bar"></div>
         <div class="footer-shield"></div>
+        <div class="built-by">Ascension Guild © 2020</a></div>
         <div class="built-by">Construído com ❤️ por <a href="https://github.com/clodbrasilino">Clodoaldo</a> - Design criado por Amanda e Hudson - Powered by <a href="https://wordpress.org/">WordPress</a></div>
       </div>
     </footer>
